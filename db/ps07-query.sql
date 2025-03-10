c2424b2550f74629f0cabc54248b19d70797439c
Phonnika Grasaekul <phonnika.grasaekul@gmail.com>
2025-03-11 00:33:58 +0700

    Remove obsolete product query from ps07-query.sql

--- db/ps07-query.sql
+++
@@ -1,6 +0,0 @@
-SELECT DISTINCT u.full_name, p.product_name
-FROM users u
-JOIN orders o ON u.user_id = o.user_id
-JOIN order_items oi ON o.order_id = oi.order_id
-JOIN products p ON oi.product_id = p.product_id
-WHERE oi.product_id = {product_id};
