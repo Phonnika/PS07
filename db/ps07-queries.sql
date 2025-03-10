2b7f8b4679f97bed4a38786867a61b1fddb581a3
Phonnika Grasaekul <phonnika.grasaekul@gmail.com>
2025-03-11 01:18:48 +0700

    Refactor database schema to replace 'orders' with 'salesorders' and update related queries and sample data accordingly

--- db/ps07-queries.sql
+++ db/ps07-queries.sql
@@ -1,23 +1,23 @@
 SELECT DISTINCT p.product_name, i.invoice_number
 FROM users u
-JOIN orders o ON u.user_id = o.user_id
-JOIN order_items oi ON o.order_id = oi.order_id
+JOIN salesorders o ON u.user_id = o.user_id
+JOIN salesorder_items oi ON o.salesorder_id = oi.salesorder_id
 JOIN products p ON oi.product_id = p.product_id
-JOIN invoices i ON o.order_id = i.order_id
+JOIN invoices i ON o.salesorder_id = i.salesorder_id
 WHERE u.user_id = {user_id};
 
 SELECT DISTINCT p.product_name, u.full_name as buyer, s.full_name as recipient
 FROM users u
-JOIN orders o ON u.user_id = o.user_id
-JOIN order_items oi ON o.order_id = oi.order_id
+JOIN salesorders o ON u.user_id = o.user_id
+JOIN salesorder_items oi ON o.salesorder_id = oi.salesorder_id
 JOIN products p ON oi.product_id = p.product_id
-JOIN shippings s ON o.order_id = s.order_id
+JOIN shippings s ON o.salesorder_id = s.salesorder_id
 WHERE oi.product_id = {product_id};
 
 SELECT u.full_name
 FROM users u
-JOIN orders o ON u.user_id = o.user_id
-JOIN order_items oi ON o.order_id = oi.order_id
-JOIN invoices i ON o.order_id = i.order_id
+JOIN salesorders o ON u.user_id = o.user_id
+JOIN salesorder_items oi ON o.salesorder_id = oi.salesorder_id
+JOIN invoices i ON o.salesorder_id = i.salesorder_id
 GROUP BY u.user_id
 HAVING SUM(oi.quantity) >= x AND SUM(i.total_amount) >= y;
--- db/ps07.sql
+++ db/ps07.sql
@@ -16,9 +16,9 @@ CREATE TABLE users (
     membership VARCHAR(50)
 );
 
--- Create Table: orders
-CREATE TABLE orders (
-    order_id INT PRIMARY KEY AUTO_INCREMENT,
+-- Create Table: salesorders
+CREATE TABLE salesorders (
+    salesorder_id INT PRIMARY KEY AUTO_INCREMENT,
     order_date DATE,
     status VARCHAR(50),
     user_id INT,
@@ -27,22 +27,21 @@ CREATE TABLE orders (
 
 -- Create Table: invoices
 CREATE TABLE invoices (
-CREATE TABLE invoices (
     invoice_id INT PRIMARY KEY AUTO_INCREMENT,
-    order_id INT UNIQUE,  -- One-to-one relationship with orders
+    salesorder_id INT UNIQUE,  -- One-to-one relationship with salesorders
     invoice_number VARCHAR(255),  -- More descriptive name
     invoice_date DATE,
     total_amount DECIMAL(10, 2),
-    FOREIGN KEY (order_id) REFERENCES orders(order_id)
+    FOREIGN KEY (salesorder_id) REFERENCES salesorders(salesorder_id)
 );
 
--- Create Table: order_items
-CREATE TABLE order_items (
-    order_item_id INT PRIMARY KEY AUTO_INCREMENT,
-    order_id INT,
+-- Create Table: salesorder_items
+CREATE TABLE salesorder_items (
+    salesorder_item_id INT PRIMARY KEY AUTO_INCREMENT,
+    salesorder_id INT,
     product_id INT,
     quantity INT,
-    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (salesorder_id) REFERENCES salesorders(salesorder_id),
     FOREIGN KEY (product_id) REFERENCES products(product_id)
 );
 
@@ -59,7 +58,7 @@ CREATE TABLE admins (
 -- Create Table: shippings
 CREATE TABLE shippings (
     shipping_id INT PRIMARY KEY AUTO_INCREMENT,
-    order_id INT, -- link to orders
+    salesorder_id INT, -- link to salesorders
     full_name VARCHAR(255),
     email_address VARCHAR(255),
     order_confirmed DATE,
@@ -67,7 +66,7 @@ CREATE TABLE shippings (
     out_for_delivery DATE,
     delivered DATE,
     expected_delivery DATE,
-     FOREIGN KEY (order_id) REFERENCES orders(order_id)
+     FOREIGN KEY (salesorder_id) REFERENCES salesorders(salesorder_id)
 );
 
 -- Create Table: reviews
@@ -78,6 +77,7 @@ CREATE TABLE reviews (
     comment TEXT,
     rating INT
 );
+
 
 -- Sample Data for Products
 INSERT INTO products (product_name, description, price) VALUES
@@ -103,42 +103,41 @@ INSERT INTO users (full_name, email_address, location, joined, membership) VALUE
 ('Mateus Cunha', 'cunha@gmail.com', 'Krabi, Thailand', '2016-10-01', 'Active'),
 ('Nzola Uemo', 'nzola@gmail.com', 'Hat Yai, Thailand', '2016-06-05', 'Pending'),
 ('Antony Mack', 'mack@gmail.com', 'Khon Kaen, Thailand', '2015-06-15', 'Pending'),
-('André da Silva', 'andré@gmail.com', 'Udon Thani, Thailand', '2018-03-13', 'Pending'),
-('Jorge Ferreira', 'jorge@gmail.com', 'Nakhon Ratchasima, Thailand', '2018-03-14', 'Active');
+('André da Silva', 'andré@gmail.com', 'Udon Thani, Thailand', '2018-03-13', 'E_Admin'),
+('Jorge Ferreira', 'jorge@gmail.com', 'Nakhon Ratchasima, Thailand', '2018-03-14', 'E_Admin');
 
-
--- Sample Data for Orders
-INSERT INTO orders (order_date, status, user_id) VALUES
-( '2023-01-15', 'Confirmed', 1),
-( '2023-01-14', 'Shipped', 2),
-( '2023-01-13', 'Delivered', 3),
-( '2023-01-12', 'Processing', 4),
-( '2023-01-11', 'Confirmed', 5),
-( '2023-01-10', 'Shipped', 6);
+-- Sample Data for salesorders
+INSERT INTO salesorders (order_date, status, user_id) VALUES
+('2023-01-15', 'Confirmed', 1),
+('2023-01-14', 'Shipped', 2),
+('2023-01-13', 'Delivered', 3),
+('2023-01-12', 'Processing', 4),
+('2023-01-11', 'Confirmed', 5),
+('2023-01-10', 'Shipped', 6);
 
 -- Sample Data for Invoices
-INSERT INTO invoices (order_id, invoice_number, invoice_date, total_amount) VALUES
-(1, 'INV-20230115-001', '2023-01-15', 3800.00),  -- Total for Order 1
-(2, 'INV-20230114-002', '2023-01-14', 5050.00),  -- Total for Order 2
-(3, 'INV-20230113-003', '2023-01-13', 2100.00),  -- Total for Order 3
-(4, 'INV-20230112-004', '2023-01-12', 6050.00),  -- Total for Order 4
-(5, 'INV-20230111-005', '2023-01-11', 4800.00),  -- Total for Order 5
-(6, 'INV-20230110-006', '2023-01-10', 2900.00);  -- Total for Order 6
+INSERT INTO invoices (salesorder_id, invoice_number, invoice_date, total_amount) VALUES
+(1, 'INV-20230115-001', '2023-01-15', 3800.00),  -- Total for salesorder 1
+(2, 'INV-20230114-002', '2023-01-14', 5050.00),  -- Total for salesorder 2
+(3, 'INV-20230113-003', '2023-01-13', 2100.00),  -- Total for salesorder 3
+(4, 'INV-20230112-004', '2023-01-12', 6050.00),  -- Total for salesorder 4
+(5, 'INV-20230111-005', '2023-01-11', 4800.00),  -- Total for salesorder 5
+(6, 'INV-20230110-006', '2023-01-10', 2900.00);  -- Total for salesorder 6
 
--- Sample Data for Order Items
-INSERT INTO order_items (order_id, product_id, quantity) VALUES
-(1, 1, 1),  -- Order 1, Off-Shoulder Top, Quantity 1
-(1, 2, 1),  -- Order 1, Biker Jacket, Quantity 1
-(2, 3, 1),  -- Order 2, Platform Boots, Quantity 1
-(2, 5, 1),  -- Order 2, Denim Shorts, Quantity 1
-(3, 6, 2),  -- Order 3, Ankle Socks, Quantity 2
-(3, 4, 1),  -- Order 3, Fuzzy Slippers, Quantity 1
-(4, 7, 1),  -- Order 4, LBD, Quantity 1
-(4, 8, 1),  -- Order 4, Cashmere Scarf, Quantity 1
-(5, 9, 1),  -- Order 5, Linen Pants, Quantity 1
-(5, 10, 1), -- Order 5, Crossbody Bag, Quantity 1
-(6, 11, 2), -- Order 6, Statement Earrings, Quantity 2
-(6, 12, 1); -- Order 6, Knit Sweater, Quantity 1
+-- Sample Data for salesorder Items
+INSERT INTO salesorder_items (salesorder_id, product_id, quantity) VALUES
+(1, 1, 1),  -- salesorder 1, Off-Shoulder Top, Quantity 1
+(1, 2, 1),  -- salesorder 1, Biker Jacket, Quantity 1
+(2, 3, 1),  -- salesorder 2, Platform Boots, Quantity 1
+(2, 5, 1),  -- salesorder 2, Denim Shorts, Quantity 1
+(3, 6, 2),  -- salesorder 3, Ankle Socks, Quantity 2
+(3, 4, 1),  -- salesorder 3, Fuzzy Slippers, Quantity 1
+(4, 7, 1),  -- salesorder 4, LBD, Quantity 1
+(4, 8, 1),  -- salesorder 4, Cashmere Scarf, Quantity 1
+(5, 9, 1),  -- salesorder 5, Linen Pants, Quantity 1
+(5, 10, 1), -- salesorder 5, Crossbody Bag, Quantity 1
+(6, 11, 2), -- salesorder 6, Statement Earrings, Quantity 2
+(6, 12, 1); -- salesorder 6, Knit Sweater, Quantity 1
 
 -- Sample Data for admins
 INSERT INTO admins (full_name, email_address, location, joined, permission) VALUES
@@ -147,13 +146,13 @@ INSERT INTO admins (full_name, email_address, location, joined, permission) VALU
 ('Alex Pfeiffer', 'alex@gmail.com', 'Phuket, Thailand', '2015-05-20', 'E_Admin'),
 ('Mike Dean', 'mike@gmail.com', 'Pattaya, Thailand', '2015-07-14', 'E_Admin'),
 ('Mateus Cunha', 'cunha@gmail.com', 'Krabi, Thailand', '2016-10-01', 'E_Admin'),
-('Nzola Uemo', 'nzola@gmail.com', 'Hat Yai, Thailand', '2016-06-05', 'E_Admin'),
-('Antony Mack', 'mack@gmail.com', 'Khon Kaen, Thailand', '2015-06-15', 'E_Admin'),
+('Nzola Uemo', 'nzola@gmail.com', 'Hat Yai, Thailand', '2016-06-05', 'Pending'),
+('Antony Mack', 'mack@gmail.com', 'Khon Kaen, Thailand', '2015-06-15', 'Pending'),
 ('André da Silva', 'andré@gmail.com', 'Udon Thani, Thailand', '2018-03-13', 'E_Admin'),
 ('Jorge Ferreira', 'jorge@gmail.com', 'Nakhon Ratchasima, Thailand', '2018-03-14', 'E_Admin');
 
 -- Sample data for shippings
-INSERT INTO shippings (order_id, full_name, email_address, order_confirmed, shipped, out_for_delivery, delivered, expected_delivery) VALUES
+INSERT INTO shippings (salesorder_id, full_name, email_address, order_confirmed, shipped, out_for_delivery, delivered, expected_delivery) VALUES
 (1, 'Leslie Maya', 'leslie@gmail.com', '2024-01-11', '2024-01-12', '2024-01-13', NULL, '2024-01-16'),
 (2, 'Alex Pfeiffer', 'alex@gmail.com', '2024-01-11', '2024-01-12', '2024-01-14', NULL, '2024-01-16'),
 (3, 'Mateus Cunha', 'cunha@gmail.com', '2024-01-11', '2024-01-13', '2024-01-15', NULL, '2024-01-17'),
@@ -166,3 +165,11 @@ INSERT INTO reviews (reviewer_name, review_date, comment, rating) VALUES
 ('Maru', '2025-02-28', 'มันพอดีกับเท้าของฉัน', 4),
 ('Abby', '2025-10-03', 'ดีมาก สวย', 5),
 ('Lisa', '2025-05-03', 'ดีมากมากค่ะ', 5);
+
+-- Update Invoice with salesorder_id. Important that you do this after insertting salesorders
+UPDATE invoices SET salesorder_id = 1 WHERE invoice_id = 1;
+UPDATE invoices SET salesorder_id = 2 WHERE invoice_id = 2;
+UPDATE invoices SET salesorder_id = 3 WHERE invoice_id = 3;
+UPDATE invoices SET salesorder_id = 4 WHERE invoice_id = 4;
+UPDATE invoices SET salesorder_id = 5 WHERE invoice_id = 5;
+UPDATE invoices SET salesorder_id = 6 WHERE invoice_id = 6;
