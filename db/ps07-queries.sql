b2812ed5a2ccd538328823c86fb1bdf21ff42c08
Phonnika Grasaekul <phonnika.grasaekul@gmail.com>
2025-03-11 00:11:37 +0700

    Refactor product query to retrieve distinct product names for a specific user

---
+++ db/ps07-queries.sql
@@ -0,0 +1,6 @@
+SELECT DISTINCT p.product_name
+FROM users u
+JOIN orders o ON u.user_id = o.user_id
+JOIN order_items oi ON o.order_id = oi.order_id
+JOIN products p ON oi.product_id = p.product_id
+WHERE u.user_id = {user_id};
