1c6497a6974d3cf3218827f37721315078601878
Phonnika Grasaekul <phonnika.grasaekul@gmail.com>
2025-03-11 00:50:57 +0700

    Enhance ps07-queries.sql to include recipient names in product queries and add a new query to filter users based on order quantity and total amount

--- db/ps07-queries.sql
+++ db/ps07-queries.sql
@@ -6,10 +6,18 @@ JOIN products p ON oi.product_id = p.product_id
 JOIN invoices i ON o.order_id = i.order_id
 WHERE u.user_id = {user_id};
 
-
-SELECT DISTINCT u.full_name, p.product_name
+SELECT DISTINCT p.product_name, u.full_name as buyer, s.full_name as recipient
 FROM users u
 JOIN orders o ON u.user_id = o.user_id
 JOIN order_items oi ON o.order_id = oi.order_id
 JOIN products p ON oi.product_id = p.product_id
+JOIN shippings s ON o.order_id = s.order_id
 WHERE oi.product_id = {product_id};
+
+SELECT u.full_name
+FROM users u
+JOIN orders o ON u.user_id = o.user_id
+JOIN order_items oi ON o.order_id = oi.order_id
+JOIN invoices i ON o.order_id = i.order_id
+GROUP BY u.user_id
+HAVING SUM(oi.quantity) >= x AND SUM(i.total_amount) >= y;
